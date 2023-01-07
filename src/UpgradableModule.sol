0375e25ecc3e814289291d89a71be07ac27e3da1
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 02:32:17 -0300

    add SubmoduleUpgraded event

--- src/UpgradableModule.sol
+++ src/UpgradableModule.sol
@@ -5,5 +5,7 @@ import {IUpgradableModule} from "./interfaces/IUpgradableModule.sol";
 import {Governable} from "./Governable.sol";
 
 contract UpgradableModule is IUpgradableModule, Governable {
+	event SubmoduleUpgraded(address previousImplementation, address newImplementation);
+
 	constructor(address _governor) Governable(_governor) {}
 }
