92a29ae0010060b323cffc71a449d867e40a4436
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 02:30:58 -0300

    UpgradableModule is Governable

--- src/UpgradableModule.sol
+++ src/UpgradableModule.sol
@@ -2,5 +2,8 @@
 pragma solidity 0.8.14;
 
 import {IUpgradableModule} from "./interfaces/IUpgradableModule.sol";
+import {Governable} from "./Governable.sol";
 
-contract UpgradableModule is IUpgradableModule {}
+contract UpgradableModule is IUpgradableModule, Governable {
+	constructor(address _governor) Governable(_governor) {}
+}
