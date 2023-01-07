da6ba85ad48f041ecd2efe957112f0547afb64ac
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 02:24:42 -0300

    feat: add onlyGovernor

--- src/Governable.sol
+++ src/Governable.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
+error OnlyGovernor();
+
 contract Governable {
 	address public governor;
+
+	modifier onlyGovernor() {
+		if (msg.sender != governor) revert OnlyGovernor();
+		_;
+	}
 
 	constructor(address _governor) {
 		governor = _governor;
