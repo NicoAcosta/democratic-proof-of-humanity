e4e0b0a7eb37c73b0bbfea5cbc8b30861706a4c1
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2023-01-07 02:16:06 -0300

    create UpgradableModule

---
+++ src/UpgradableModule.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import {IUpgradableModule} from "./interfaces/IUpgradableModule.sol";
+
+contract UpgradableModule is IUpgradableModule {}
